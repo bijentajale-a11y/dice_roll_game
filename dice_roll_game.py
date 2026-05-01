e9a42f18e0a6d08eb33cf051e69de7c8bf4025ab
Bijen Tajale <bijentajale@gmail.com>
2026-05-01 13:24:23 +0545

    Featues of roll count and total score added.

--- dice_roll_game.py
+++ dice_roll_game.py
@@ -1,14 +1,35 @@
 import random
 
+total_score = 0
+roll_count = 0
+target = 8
+
 while True:
     choice=input("Roll the dice(y/n): ").lower()
     if choice == 'y':
-        dice1=random.randint(1,6)
-        dice2=random.randint(1,6)
-        print(f"({dice1},{dice2})")
+        roll_count += 1 #roll counter
+
+        die1=random.randint(1,6)
+        die2=random.randint(1,6)
+
+        total = die1 + die2
+
+        total_score += total
+        
+        print(f"\nRoll {roll_count}:({die1},{die2})")
+        print(f"Current roll total:{total}")
+        
+        if total == target:
+            print("You win!")
+        else:
+            print("You lose!")
+        print(f"Total Score so far: {total_score}\n")
+    
 
     elif choice == 'n':
-        print("Thanks for playing!")
+        print("\nThanks for playing!")
+        print(f"Total rolls: {roll_count}")
+        print(f"Final score: {total_score}")
         break
 
     else:
