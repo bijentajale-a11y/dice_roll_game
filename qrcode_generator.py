04d6f9a9ca33406846d34ea79376e0f9102ce940
Bijen Tajale <bijentajale@gmail.com>
2026-05-03 21:58:16 +0545

    Simple QR code generator

---
+++ qrcode_generator.py
@@ -0,0 +1,10 @@
+import qrcode
+
+data=input("Enter the text or URL:").strip()#To get rid of any extra spaces
+filename=input("Enter the file name:").strip()
+
+qr=qrcode.QRCode(box_size=10,border=4)
+qr.add_data(data)
+image=qr.make_image(fill_color='black',back_color='white')
+image.save(filename)
+print(f"QR generated successfully as {filename}.")
